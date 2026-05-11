357369c1ac1f667c65a59afc8dbc2384126ed273
Amandine <Amandine@LAPTOP-MIRANDIN>
2026-05-11 11:15:49 +0200

    Fix taille grpBox Tableau bord (elle était pas de la même taille que les autres)

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,23 +29,23 @@
         private void InitializeComponent()
         {
             this.tabMenu = new System.Windows.Forms.TabControl();
-            this.tabPagePrincipal = new System.Windows.Forms.TabPage();
-            this.btnDecouvRaces = new System.Windows.Forms.Button();
-            this.btnNouvMission = new System.Windows.Forms.Button();
-            this.BtnInfosPlan = new System.Windows.Forms.Button();
-            this.grpTableauBord = new System.Windows.Forms.GroupBox();
-            this.grpDecouvRaces = new System.Windows.Forms.GroupBox();
-            this.grpInfosPlan = new System.Windows.Forms.GroupBox();
-            this.grpNouvMission = new System.Windows.Forms.GroupBox();
             this.tabPageMenu = new System.Windows.Forms.TabPage();
             this.btnTableauBord = new System.Windows.Forms.Button();
             this.btnDecouverteRaces = new System.Windows.Forms.Button();
             this.btnNouvelleMission = new System.Windows.Forms.Button();
             this.btnInfosPlanete = new System.Windows.Forms.Button();
+            this.tabPagePrincipal = new System.Windows.Forms.TabPage();
             this.btnTbBord = new System.Windows.Forms.Button();
+            this.btnDecouvRaces = new System.Windows.Forms.Button();
+            this.btnNouvMission = new System.Windows.Forms.Button();
+            this.BtnInfosPlan = new System.Windows.Forms.Button();
+            this.grpTableauBord = new System.Windows.Forms.GroupBox();
+            this.grpDecouvRaces = new System.Windows.Forms.GroupBox();
+            this.grpNouvMission = new System.Windows.Forms.GroupBox();
+            this.grpInfosPlan = new System.Windows.Forms.GroupBox();
             this.tabMenu.SuspendLayout();
-            this.tabPagePrincipal.SuspendLayout();
             this.tabPageMenu.SuspendLayout();
+            this.tabPagePrincipal.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabMenu
@@ -57,110 +57,6 @@
             this.tabMenu.SelectedIndex = 0;
             this.tabMenu.Size = new System.Drawing.Size(1289, 927);
             this.tabMenu.TabIndex = 4;
-            // 
-            // tabPagePrincipal
-            // 
-            this.tabPagePrincipal.BackColor = System.Drawing.Color.Black;
-            this.tabPagePrincipal.Controls.Add(this.btnTbBord);
-            this.tabPagePrincipal.Controls.Add(this.btnDecouvRaces);
-            this.tabPagePrincipal.Controls.Add(this.btnNouvMission);
-            this.tabPagePrincipal.Controls.Add(this.BtnInfosPlan);
-            this.tabPagePrincipal.Controls.Add(this.grpTableauBord);
-            this.tabPagePrincipal.Controls.Add(this.grpDecouvRaces);
-            this.tabPagePrincipal.Controls.Add(this.grpNouvMission);
-            this.tabPagePrincipal.Controls.Add(this.grpInfosPlan);
-            this.tabPagePrincipal.Location = new System.Drawing.Point(4, 29);
-            this.tabPagePrincipal.Name = "tabPagePrincipal";
-            this.tabPagePrincipal.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPagePrincipal.Size = new System.Drawing.Size(1281, 894);
-            this.tabPagePrincipal.TabIndex = 1;
-            this.tabPagePrincipal.Text = "tabPage2";
-            // 
-            // btnDecouvRaces
-            // 
-            this.btnDecouvRaces.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnDecouvRaces;
-            this.btnDecouvRaces.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnDecouvRaces.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.btnDecouvRaces.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
-            this.btnDecouvRaces.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
-            this.btnDecouvRaces.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnDecouvRaces.Location = new System.Drawing.Point(35, 273);
-            this.btnDecouvRaces.Name = "btnDecouvRaces";
-            this.btnDecouvRaces.Size = new System.Drawing.Size(225, 150);
-            this.btnDecouvRaces.TabIndex = 1;
-            this.btnDecouvRaces.UseVisualStyleBackColor = true;
-            this.btnDecouvRaces.Click += new System.EventHandler(this.btnDecouvRaces_Click);
-            // 
-            // btnNouvMission
-            // 
-            this.btnNouvMission.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnNouvMission;
-            this.btnNouvMission.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.btnNouvMission.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.btnNouvMission.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
-            this.btnNouvMission.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
-            this.btnNouvMission.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNouvMission.Location = new System.Drawing.Point(35, 421);
-            this.btnNouvMission.Name = "btnNouvMission";
-            this.btnNouvMission.Size = new System.Drawing.Size(225, 150);
-            this.btnNouvMission.TabIndex = 2;
-            this.btnNouvMission.UseVisualStyleBackColor = true;
-            this.btnNouvMission.Click += new System.EventHandler(this.btnNouvMission_Click);
-            // 
-            // BtnInfosPlan
-            // 
-            this.BtnInfosPlan.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnInfosPlanete;
-            this.BtnInfosPlan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.BtnInfosPlan.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
-            this.BtnInfosPlan.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
-            this.BtnInfosPlan.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
-            this.BtnInfosPlan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnInfosPlan.Location = new System.Drawing.Point(35, 569);
-            this.BtnInfosPlan.Name = "BtnInfosPlan";
-            this.BtnInfosPlan.Size = new System.Drawing.Size(225, 150);
-            this.BtnInfosPlan.TabIndex = 3;
-            this.BtnInfosPlan.UseVisualStyleBackColor = true;
-            this.BtnInfosPlan.Click += new System.EventHandler(this.BtnInfosPlan_Click);
-            // 
-            // grpTableauBord
-            // 
-            this.grpTableauBord.BackColor = System.Drawing.Color.Black;
-            this.grpTableauBord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.grpTableauBord.Location = new System.Drawing.Point(290, 20);
-            this.grpTableauBord.Name = "grpTableauBord";
-            this.grpTableauBord.Size = new System.Drawing.Size(950, 825);
-            this.grpTableauBord.TabIndex = 4;
-            this.grpTableauBord.TabStop = false;
-            this.grpTableauBord.Visible = false;
-            // 
-            // grpDecouvRaces
-            // 
-            this.grpDecouvRaces.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.grpDecouvRaces.Location = new System.Drawing.Point(290, 20);
-            this.grpDecouvRaces.Name = "grpDecouvRaces";
-            this.grpDecouvRaces.Size = new System.Drawing.Size(955, 828);
-            this.grpDecouvRaces.TabIndex = 5;
-            this.grpDecouvRaces.TabStop = false;
-            this.grpDecouvRaces.Visible = false;
-            // 
-            // grpInfosPlan
-            // 
-            this.grpInfosPlan.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.grpInfosPlan.Location = new System.Drawing.Point(290, 20);
-            this.grpInfosPlan.Name = "grpInfosPlan";
-            this.grpInfosPlan.Size = new System.Drawing.Size(955, 828);
-            this.grpInfosPlan.TabIndex = 6;
-            this.grpInfosPlan.TabStop = false;
-            this.grpInfosPlan.Visible = false;
-            // 
-            // grpNouvMission
-            // 
-            this.grpNouvMission.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.grpNouvMission.Location = new System.Drawing.Point(290, 20);
-            this.grpNouvMission.Name = "grpNouvMission";
-            this.grpNouvMission.Size = new System.Drawing.Size(955, 828);
-            this.grpNouvMission.TabIndex = 6;
-            this.grpNouvMission.TabStop = false;
-            this.grpNouvMission.Visible = false;
             // 
             // tabPageMenu
             // 
@@ -263,6 +159,24 @@
             this.btnInfosPlanete.UseVisualStyleBackColor = false;
             this.btnInfosPlanete.Click += new System.EventHandler(this.btnInfosPlanete_Click);
             // 
+            // tabPagePrincipal
+            // 
+            this.tabPagePrincipal.BackColor = System.Drawing.Color.Black;
+            this.tabPagePrincipal.Controls.Add(this.btnTbBord);
+            this.tabPagePrincipal.Controls.Add(this.btnDecouvRaces);
+            this.tabPagePrincipal.Controls.Add(this.btnNouvMission);
+            this.tabPagePrincipal.Controls.Add(this.BtnInfosPlan);
+            this.tabPagePrincipal.Controls.Add(this.grpTableauBord);
+            this.tabPagePrincipal.Controls.Add(this.grpDecouvRaces);
+            this.tabPagePrincipal.Controls.Add(this.grpNouvMission);
+            this.tabPagePrincipal.Controls.Add(this.grpInfosPlan);
+            this.tabPagePrincipal.Location = new System.Drawing.Point(4, 29);
+            this.tabPagePrincipal.Name = "tabPagePrincipal";
+            this.tabPagePrincipal.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPagePrincipal.Size = new System.Drawing.Size(1281, 894);
+            this.tabPagePrincipal.TabIndex = 1;
+            this.tabPagePrincipal.Text = "tabPage2";
+            // 
             // btnTbBord
             // 
             this.btnTbBord.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnTableauBord;
@@ -278,6 +192,92 @@
             this.btnTbBord.UseVisualStyleBackColor = true;
             this.btnTbBord.Click += new System.EventHandler(this.btnTbBord_Click);
             // 
+            // btnDecouvRaces
+            // 
+            this.btnDecouvRaces.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnDecouvRaces;
+            this.btnDecouvRaces.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnDecouvRaces.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnDecouvRaces.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
+            this.btnDecouvRaces.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
+            this.btnDecouvRaces.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDecouvRaces.Location = new System.Drawing.Point(35, 273);
+            this.btnDecouvRaces.Name = "btnDecouvRaces";
+            this.btnDecouvRaces.Size = new System.Drawing.Size(225, 150);
+            this.btnDecouvRaces.TabIndex = 1;
+            this.btnDecouvRaces.UseVisualStyleBackColor = true;
+            this.btnDecouvRaces.Click += new System.EventHandler(this.btnDecouvRaces_Click);
+            // 
+            // btnNouvMission
+            // 
+            this.btnNouvMission.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnNouvMission;
+            this.btnNouvMission.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnNouvMission.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.btnNouvMission.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
+            this.btnNouvMission.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
+            this.btnNouvMission.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNouvMission.Location = new System.Drawing.Point(35, 421);
+            this.btnNouvMission.Name = "btnNouvMission";
+            this.btnNouvMission.Size = new System.Drawing.Size(225, 150);
+            this.btnNouvMission.TabIndex = 2;
+            this.btnNouvMission.UseVisualStyleBackColor = true;
+            this.btnNouvMission.Click += new System.EventHandler(this.btnNouvMission_Click);
+            // 
+            // BtnInfosPlan
+            // 
+            this.BtnInfosPlan.BackgroundImage = global::SAE24STARGATE.Properties.Resources.btnInfosPlanete;
+            this.BtnInfosPlan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BtnInfosPlan.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.BtnInfosPlan.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
+            this.BtnInfosPlan.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(31)))), ((int)(((byte)(234)))), ((int)(((byte)(204)))));
+            this.BtnInfosPlan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnInfosPlan.Location = new System.Drawing.Point(35, 569);
+            this.BtnInfosPlan.Name = "BtnInfosPlan";
+            this.BtnInfosPlan.Size = new System.Drawing.Size(225, 150);
+            this.BtnInfosPlan.TabIndex = 3;
+            this.BtnInfosPlan.UseVisualStyleBackColor = true;
+            this.BtnInfosPlan.Click += new System.EventHandler(this.BtnInfosPlan_Click);
+            // 
+            // grpTableauBord
+            // 
+            this.grpTableauBord.BackColor = System.Drawing.Color.Black;
+            this.grpTableauBord.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grpTableauBord.Location = new System.Drawing.Point(290, 20);
+            this.grpTableauBord.Name = "grpTableauBord";
+            this.grpTableauBord.Size = new System.Drawing.Size(950, 825);
+            this.grpTableauBord.TabIndex = 4;
+            this.grpTableauBord.TabStop = false;
+            this.grpTableauBord.Visible = false;
+            // 
+            // grpDecouvRaces
+            // 
+            this.grpDecouvRaces.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grpDecouvRaces.Location = new System.Drawing.Point(290, 20);
+            this.grpDecouvRaces.Name = "grpDecouvRaces";
+            this.grpDecouvRaces.Size = new System.Drawing.Size(950, 825);
+            this.grpDecouvRaces.TabIndex = 5;
+            this.grpDecouvRaces.TabStop = false;
+            this.grpDecouvRaces.Visible = false;
+            // 
+            // grpNouvMission
+            // 
+            this.grpNouvMission.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grpNouvMission.Location = new System.Drawing.Point(290, 20);
+            this.grpNouvMission.Name = "grpNouvMission";
+            this.grpNouvMission.Size = new System.Drawing.Size(950, 825);
+            this.grpNouvMission.TabIndex = 6;
+            this.grpNouvMission.TabStop = false;
+            this.grpNouvMission.Visible = false;
+            // 
+            // grpInfosPlan
+            // 
+            this.grpInfosPlan.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.grpInfosPlan.Location = new System.Drawing.Point(290, 20);
+            this.grpInfosPlan.Name = "grpInfosPlan";
+            this.grpInfosPlan.Size = new System.Drawing.Size(950, 825);
+            this.grpInfosPlan.TabIndex = 6;
+            this.grpInfosPlan.TabStop = false;
+            this.grpInfosPlan.Visible = false;
+            // 
             // frmAccueil
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
@@ -292,8 +292,8 @@
             this.Text = "Gestion des missions";
             this.Load += new System.EventHandler(this.frmAccueil_Load);
             this.tabMenu.ResumeLayout(false);
-            this.tabPagePrincipal.ResumeLayout(false);
             this.tabPageMenu.ResumeLayout(false);
+            this.tabPagePrincipal.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
